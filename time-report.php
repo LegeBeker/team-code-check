aee695ae2bb9423abe7c92cf118067503155f0e0
Volkan Welp <volkanwelp@gmail.com>
2023-04-14 21:20:11 +0200

    💄 Improve layout and remove graph

--- time-report.php
+++ time-report.php
@@ -40,7 +40,83 @@
                     </form>
                 </div>
             </div>
-
+            <div class="content-wrapper">
+                <div class="content">
+                    <h1>Time Report</h1>
+                    <form action="forms/submit.php" method="post">
+                        <div class="form-group">
+                            <label for="start-time">Person:</label>
+                            <select id="person" name="person" class="form-control" required>
+                                <option value="" disabled selected>Select a person</option>
+                                <?php foreach ($persons as $person) { ?>
+                                    <option value="<?php echo $person; ?>"><?php echo ucfirst($person); ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="type">Type:</label>
+                            <select id="type" name="type" class="form-control" required>
+                                <option value="" disabled selected>Select a type</option>
+                                <?php foreach ($types as $type) { ?>
+                                    <option value="<?php echo $type; ?>"><?php echo ucfirst($type); ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="start-time">Start Time:</label>
+                            <input type="datetime-local" id="start-time" name="start_time" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="end-time">End Time: (if not filled, it will be running)</label>
+                            <input type="datetime-local" id="end-time" name="end_time" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="branch">Branch:</label>
+                            <select id="branch" name="branch" class="form-control" required>
+                                <option value="" disabled selected>Select a branch</option>
+                                <?php foreach (fetchAllBranches($accessToken) as $branch) { ?>
+                                    <option value="<?php echo $branch['name']; ?>"><?php echo $branch['name']; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">comment:</label>
+                            <input type="text" id="comment" name="comment" class="form-control" maxlength="50">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-7">
+            <div class="content-wrapper">
+                <div class="content">
+                    <h1>Running Timers</h1>
+                    <table class="table table-hover">
+                        <tbody>
+                            <?php
+                            if (count(getRunningTimers($timeReports)) == 0) {
+                                echo '<tr><td colspan="4" class="text-muted text-center">No running timers</td></tr>';
+                            }
+                            foreach (getRunningTimers($timeReports) as $runningTimer) { ?>
+                                <form action="forms/stop-timer.php" method="post">
+                                    <tr>
+                                        <td class="align-middle"><?php echo ucfirst($runningTimer['person']); ?></td>
+                                        <td class="align-middle"><?php echo $runningTimer['type']; ?></td>
+                                        <td class="align-middle"><?php echo $runningTimer['start']; ?></td>
+                                        <td class="align-middle"><?php echo $runningTimer['branch']; ?></td>
+                                        <td class="align-middle text-right">
+                                            <input name="comment" id="comment<?php echo $runningTimer['id']; ?>" type="hidden">
+                                            <input name="timer_id" value="<?php echo $runningTimer['id']; ?>" type="hidden">
+                                            <button name="end_timer" data-timer_id="<?php echo $runningTimer['id']; ?>" class="btn btn-danger end-timer">End Timer</button>
+                                        </td>
+                                    </tr>
+                                </form>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
             <div class="content-wrapper">
                 <div class="content">
                     <h1>Last reports</h1>
@@ -88,7 +164,7 @@
                                         </td>
                                     </tr>
                                 <?php
-                                    if ($i == 5) {
+                                    if ($i == 7) {
                                         break;
                                     }
                                 }
@@ -99,90 +175,6 @@
                             </tbody>
                         </table>
                     </div>
-                </div>
-            </div>
-
-            <div class="content-wrapper">
-                <div class="content">
-                    <h1>Cumulative Hours</h1>
-                    <canvas id="timeReportChart"></canvas>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-7">
-            <div class="content-wrapper">
-                <div class="content">
-                    <h1>Running Timers</h1>
-                    <table class="table table-hover">
-                        <tbody>
-                            <?php
-                            if (count(getRunningTimers($timeReports)) == 0) {
-                                echo '<tr><td colspan="4" class="text-muted text-center">No running timers</td></tr>';
-                            }
-                            foreach (getRunningTimers($timeReports) as $runningTimer) { ?>
-                                <form action="forms/stop-timer.php" method="post">
-                                    <tr>
-                                        <td class="align-middle"><?php echo ucfirst($runningTimer['person']); ?></td>
-                                        <td class="align-middle"><?php echo $runningTimer['type']; ?></td>
-                                        <td class="align-middle"><?php echo $runningTimer['start']; ?></td>
-                                        <td class="align-middle"><?php echo $runningTimer['branch']; ?></td>
-                                        <td class="align-middle text-right">
-                                            <input name="comment" id="comment<?php echo $runningTimer['id']; ?>" type="hidden">
-                                            <input name="timer_id" value="<?php echo $runningTimer['id']; ?>" type="hidden">
-                                            <button name="end_timer" data-timer_id="<?php echo $runningTimer['id']; ?>" class="btn btn-danger end-timer">End Timer</button>
-                                        </td>
-                                    </tr>
-                                </form>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="content-wrapper">
-                <div class="content">
-                    <h1>Time Report</h1>
-                    <form action="forms/submit.php" method="post">
-                        <div class="form-group">
-                            <label for="start-time">Person:</label>
-                            <select id="person" name="person" class="form-control" required>
-                                <option value="" disabled selected>Select a person</option>
-                                <?php foreach ($persons as $person) { ?>
-                                    <option value="<?php echo $person; ?>"><?php echo ucfirst($person); ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="type">Type:</label>
-                            <select id="type" name="type" class="form-control" required>
-                                <option value="" disabled selected>Select a type</option>
-                                <?php foreach ($types as $type) { ?>
-                                    <option value="<?php echo $type; ?>"><?php echo ucfirst($type); ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="start-time">Start Time:</label>
-                            <input type="datetime-local" id="start-time" name="start_time" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="end-time">End Time: (if not filled, it will be running)</label>
-                            <input type="datetime-local" id="end-time" name="end_time" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="branch">Branch:</label>
-                            <select id="branch" name="branch" class="form-control" required>
-                                <option value="" disabled selected>Select a branch</option>
-                                <?php foreach (fetchAllBranches($accessToken) as $branch) { ?>
-                                    <option value="<?php echo $branch['name']; ?>"><?php echo $branch['name']; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="comment">comment:</label>
-                            <input type="text" id="comment" name="comment" class="form-control" maxlength="50">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
                 </div>
             </div>
         </div>
